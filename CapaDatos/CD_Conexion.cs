1ccd33bda375ea608874f138086603c92564689b
Oscar Zambrano <ferguspawn@gmail.com>
2021-04-01 19:36:20 -0300

    Agregar archivos de proyecto.

---
+++ CapaDatos/CD_Conexion.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.SqlTy;
+using System.Data;
+
+
+namespace CapaDatos
+{
+    public class CD_Conexion
+    {
+        private SqlConnection Conexion = new SqlConnection("Server=DESKTOP-UEPK13H\\RONETJOHN;DataBase= Practica;Integrated Security=true");
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
+    }
+}
---
+++ CapaDatos/CD_Productos.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Data.S
+
+namespace CapaDatos
+{
+    class CD_Productos
+    {
+    }
+}
